fd31f028498f5231eb1800531fea39f5f4a14e47
JohanOelgaard <johan.oelgaard@gmail.com>
2024-04-26 14:53:26 -0700

    Cleaned up the project directory so it is ready for submission

--- proj/proj03/proj03.py
+++ proj/proj03/proj03.py
@@ -49,14 +49,5 @@ def cgmwildboot(data, model, n_bootstraps, cluster, bootcluster, seed=1234):
                 pval = np.mean(np.abs(w_boot) > np.abs(w))
                 b_pvals.append(pval)
 
-                # wrong way still assuming normal or student's-t distribution
-                # t = b_ests[:,i].mean() / b_ests[:,i].std()
-                # using the cluster size to determine whether to use normal or t distribution
-                # if df[cluster].nunique() >= 30:
-                #     b_pval = 2 * (1 - stats.norm.cdf(np.abs(t)))
-                # else:
-                #        b_pval = 2 * (1 - stats.t.cdf(np.abs(t),df=df[cluster].nunique()-1))
-                # b_pvals.append(b_pval)
-
         return b_ests, b_pvals
         
